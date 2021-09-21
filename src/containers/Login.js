9fc4be6fd7ad6dafef8be2aedce313f59e4f1a22
elise <rousseaujumelle@gmail.com>
2021-09-21 19:34:17 +0200

    bug report login - remplace 'employee' par 'admin

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -31,12 +31,12 @@ export default class Login {
     PREVIOUS_LOCATION = this.PREVIOUS_LOCATION
     this.document.body.style.backgroundColor="#fff"
   }
-
+  //erreur trouvée 
   handleSubmitAdmin = e => {
     const user = {
       type: "Admin",
-      email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
-      password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+      email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
+      password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       status: "connected"
     }
     this.localStorage.setItem("user", JSON.stringify(user))
